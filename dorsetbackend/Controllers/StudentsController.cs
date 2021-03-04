c2b89e8b5948bcb4620ac6dd425820631e9bed50
ilias belkhder <23727@student.dorset-college.ie>
2021-03-04 20:26:11 +0000

    student

--- dorsetbackend/Controllers/StudentsController.cs
+++ dorsetbackend/Controllers/StudentsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TestApplication.Data;
 using TestApplication.DTO;
+using TestApplication.Models;
 
 namespace TestApplication.Controllers
 {
@@ -15,59 +16,133 @@ namespace TestApplication.Controllers
     {
 
 
-        
+
         private readonly Context _context;
 
-            public StudentsController(Context context)
+        public StudentsController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentDTO>>> GetStudents()
+        {
+            var Student = from student in _context.student
+                          join student_info in _context.student_info on student.student_id equals student_info.student_id
+                          select new StudentDTO
+                          {
+                              student_id = student.student_id,
+                              age = student.age,
+                              firstName = student.firstName,
+                              lastName = student.lastName,
+                              adress = student_info.adress,
+                              country = student_info.country,
+                              grade = student_info.grade
+
+                          };
+
+            return await Student.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<StudentDTO> GetStudents_byId(int id)
+        {
+            var Student = from student in _context.student
+                          join student_info in _context.student_info on student.student_id equals student_info.student_id
+                          select new StudentDTO
+                          {
+                              student_id = student.student_id,
+                              age = student.age,
+                              firstName = student.firstName,
+                              lastName = student.lastName,
+                              adress = student_info.adress,
+                              country = student_info.country,
+                              grade = student_info.grade
+
+                          };
+
+            var Student_by_id = Student.ToList().Find(x => x.student_id == id);
+
+            if (Student_by_id == null)
             {
-                _context = context;
+                return NotFound();
+            }
+            return Student_by_id;
+        }
+        [HttpPost]
+        public async Task<ActionResult<StudentDTO>> Add_students(AddStudent studentDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
             }
 
-            [HttpGet]
-            public async Task<ActionResult<IEnumerable<StudentDTO>>> GetStudents()
+            var student = new Student()
             {
-                var Student = from student in _context.student
-                              join student_info in _context.student_info on student.student_id equals student_info.student_id
-                              select new StudentDTO
-                              {
-                                  student_id = student.student_id,
-                                  age = student.age,
-                                  firstName = student.firstName,
-                                  lastName = student.lastName,
-                                  adress = student_info.adress,
-                                  country = student_info.country,
-                                  grade = student_info.grade
+                age = studentDTO.age,
+                firstName = studentDTO.firstName,
+                lastName = studentDTO.lastName
+            };
 
-                           };
+            await _context.student.AddAsync(student);
+            await _context.SaveChangesAsync();
 
-                return await Student.ToListAsync();
+            var student_info = new Student_info()
+            {
+                country = studentDTO.country,
+                adress = studentDTO.adress,
+                grade = studentDTO.grade
+            };
+            await _context.AddAsync(student_info);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("Getstudents", new { id = student.id }, studentDTO);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> Delete_student(int id)
+        {
+            var student = _context.student.Find(id);
+            var student_description = _context.student_info.SingleOrDefault(x => x.student_id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Remove(student);
+                _context.Remove(student_description);
+                await _context.SaveChangesAsync();
+                return student;
             }
 
-            [HttpGet("{id}")]
-            public ActionResult<StudentDTO> GetStudents_byId(int id)
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update_students(int id, StudentDTO student)
+        {
+            if (id != student.student_id || !studentExists(id))
             {
-                var Student = from student in _context.student
-                              join student_info in _context.student_info on student.student_id equals student_info.student_id
-                              select new StudentDTO
-                              {
-                                  student_id = student.student_id,
-                                  age = student.age,
-                                  firstName = student.firstName,
-                                  lastName = student.lastName,
-                                  adress = student_info.adress,
-                                  country = student_info.country,
-                                  grade = student_info.grade
-
-                              };
-
-                var Student_by_id = Student.ToList().Find(x => x.student_id == id);
-
-                if (Student_by_id == null)
-                {
-                    return NotFound();
-                }
-                return Student_by_id;
+                return BadRequest();
+            }
+            else
+            {
+                var students = _context.student.SingleOrDefault(x => x.id == id);
+                var students_info = _context.student_info.SingleOrDefault(x => x.student_id == id);
+                students.id = student.student_id;
+                students.age = student.age;
+                students.firstName = student.firstName;
+                students.lastName = student.lastName;
+                students_info.country = student.country;
+                students_info.grade = student.grade;
+                students_info.adress = student.adress;
+                await _context.SaveChangesAsync();
+                return NoContent();
             }
         }
+        private bool studentExists(int id)
+        {
+            return _context.student.Any(x => x.id == id);
+        }
     }
+}
 
---
+++ dorsetbackend/DTO/AddStudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApplication.DTO
+{
+    public class AddStudent
+    {
+        [Required]
+        public int age { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string country { get; set; }
+        public int grade { get; set; }
+        public string adress { get; set; }
+    }
+}
